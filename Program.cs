747b8681e4496a6a0a6971e6bd4b7f18476c5b1c
Soumya Sutar <soumsutar9@gmail.com>
2023-11-09 15:20:03 +0530

    VariableAndDataType

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableAndDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Soumya";
+            int userAge = 24;
+            double userHeight = 169;            
+            Console.WriteLine("Name: " + userName);
+            Console.WriteLine("Age: " + userAge);
+            Console.WriteLine("Height: " + userHeight + " cm");
+        }
+    }
+}
